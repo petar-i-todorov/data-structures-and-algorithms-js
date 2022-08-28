ee32cfd46b4507b9a2d108be6839225992fe382a
Petar Ivanov <petarmedicine@gmail.com>
2022-08-28 15:50:02 +0300

    adding breadth-first search

--- data-structures-and-algorithms.js
+++ data-structures-and-algorithms.js
@@ -148,3 +148,59 @@ console.log(hashTable.get("dog"));
 console.log(hashTable.get("god"));
 //worst case of operations with the data - O(n) - depending on the count of collisions. Better hash function = less collisions = less complexity
 //average case - O(1) if we choose the right algorithm for the hash function
+
+//Graphs. Breadth-first search
+
+// const hashFunction = (key, tableSize) => {
+//   let hash = 15;
+//   for (let i = 0; i < key.length; i++) {
+//     hash = (13 * hash * key.charCodeAt(i)) % tableSize;
+//   }
+//   return hash;
+// };
+// class HashTable {
+//   table = new Array(3000);
+//   set = (key, value) => {
+//     const idx = hashFunction(key, this.table.length);
+//     if (!this.table[idx]) {
+//       this.table[idx] = [[key, value]];
+//     } else {
+//       this.table[idx].push([key, value]);
+//     }
+//   };
+//   get = (key) => {
+//     const idx = hashFunction(key, this.table.length);
+//     return this.table[idx].find((el) => el[0] === key)[1];
+//   };
+// }
+const graph = new HashTable();
+graph.set("id", "sg");
+graph.set("id", "ph");
+graph.set("id", "my");
+graph.set("my", "kh");
+graph.set("kh", "cn");
+function breadthFirstSearch(rootKey, table, searchVal) {
+  const queue = [rootKey];
+
+  while (queue.length > 0) {
+    if (queue[0] === searchVal) {
+      return queue[0];
+    }
+    const idx = hashFunction(queue[0], table.length);
+    // add all child nodes to queue
+    if (table[idx]) {
+      //if condition because not all countries are included as indexes. For example ph in this case is a value of a key but it's not a key itself and it's not reserving an index in the array for itself
+      table[idx]
+        .filter((pair) => pair[0] === queue[0])
+        .forEach((pair) => queue.push(pair[1]));
+    }
+
+    // remove element that we just searched
+    queue.shift();
+    console.log("queue after shifting: " + queue);
+  }
+
+  // didn't find a value in the tree
+  return null;
+}
+console.log(breadthFirstSearch("id", hashTable.table, "cn"));
