c0bda7a59351bed02cacf452d2aa1996266951f2
Petar Ivanov <petarmedicine@gmail.com>
2022-08-29 23:16:44 +0300

    function memoization added

--- data-structures-and-algorithms.js
+++ data-structures-and-algorithms.js
@@ -214,54 +214,77 @@
 //complexity - between O(1) and O(V + E)
 
 //Graphs. Dijkstra's algorithm --------------------------------------------------------------------------------------------------------
-const graph = {};
-graph.a = { b: 2, c: 1 };
-graph.b = { f: 7 };
-graph.c = { d: 5, e: 2 };
-graph.d = { f: 2 };
-graph.e = { f: 1 };
-graph.f = { g: 1 };
-graph.g = {};
+// const graph = {};
+// graph.a = { b: 2, c: 1 };
+// graph.b = { f: 7 };
+// graph.c = { d: 5, e: 2 };
+// graph.d = { f: 2 };
+// graph.e = { f: 1 };
+// graph.f = { g: 1 };
+// graph.g = {};
 
-const findLowestCostNode = (costs, processed) => {
-  let lowestCost = Infinity;
-  let lowestCostNode;
-  Object.keys(costs).forEach((node) => {
-    if (!processed.includes(node)) {
-      if (costs[node] < lowestCost) {
-        lowestCost = costs[node];
-        lowestCostNode = node;
-      }
-    }
-  });
-  return lowestCostNode;
-};
+// const findLowestCostNode = (costs, processed) => {
+//   let lowestCost = Infinity;
+//   let lowestCostNode;
+//   Object.keys(costs).forEach((node) => {
+//     if (!processed.includes(node)) {
+//       if (costs[node] < lowestCost) {
+//         lowestCost = costs[node];
+//         lowestCostNode = node;
+//       }
+//     }
+//   });
+//   return lowestCostNode;
+// };
 
-const shortestPath = (startNode, graph) => {
-  const costs = {};
-  const processed = [];
-  Object.keys(graph).forEach((node) => {
-    if (node !== startNode) {
-      costs[node] = graph[startNode][node] || Infinity;
-    }
-  });
-  processed.push(startNode);
-  while (processed.length <= Object.keys(graph).length) {
-    const lowestCostNode = findLowestCostNode(costs, processed);
-    const neighbors = graph[lowestCostNode];
-    if (neighbors) {
-      Object.keys(neighbors).forEach((node) => {
-        if (!processed.includes(node)) {
-          const newCost = costs[lowestCostNode] + graph[lowestCostNode][node];
-          if (newCost < costs[node]) {
-            costs[node] = newCost;
-          }
-        }
-      });
-    }
-    processed.push(lowestCostNode);
-  }
-  return costs;
-};
-console.log(shortestPath("a", graph));
+// const shortestPath = (startNode, graph) => {
+//   const costs = {};
+//   const processed = [];
+//   Object.keys(graph).forEach((node) => {
+//     if (node !== startNode) {
+//       costs[node] = graph[startNode][node] || Infinity;
+//     }
+//   });
+//   processed.push(startNode);
+//   while (processed.length <= Object.keys(graph).length) {
+//     const lowestCostNode = findLowestCostNode(costs, processed);
+//     const neighbors = graph[lowestCostNode];
+//     if (neighbors) {
+//       Object.keys(neighbors).forEach((node) => {
+//         if (!processed.includes(node)) {
+//           const newCost = costs[lowestCostNode] + graph[lowestCostNode][node];
+//           if (newCost < costs[node]) {
+//             costs[node] = newCost;
+//           }
+//         }
+//       });
+//     }
+//     processed.push(lowestCostNode);
+//   }
+//   return costs;
+// };
+// console.log(shortestPath("a", graph));
 // //complexity - O(V^2)
+
+//function memoization
+const memoizedFunction = (fn) => {
+  const cash = {};
+  return function (n) {
+    if (cash[n]) {
+      console.log("Previously counted result: " + cash[n]);
+      return cash[n];
+    }
+    let result = fn(n);
+    cash[n] = result;
+    return result;
+  };
+};
+const fibonacci = (n) => {
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+const memoizedFibonacci = memoizedFunction(fibonacci);
+console.log(memoizedFibonacci(10));
+console.log(memoizedFibonacci(10));
